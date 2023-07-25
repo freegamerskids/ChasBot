8f49ca0b96a41cd1b6626a5f3991997b321efed0
freegamerskids <freegamerskids@gmail.com>
2023-07-25 23:04:52 +0200

    quick fix + added some gifs

--- src/cmds/util/uptime.ts
+++ src/cmds/util/uptime.ts
@@ -8,6 +8,6 @@ export class Uptime {
     }
 
     static get hmsString(): string {
-        return `The bot has been up for ${Math.floor(Uptime.uptimeSeconds / 60 / 60 / 24)} day(s), ${Math.floor(Uptime.uptimeSeconds / 60 / 60)} hour(s), ${Math.floor((Uptime.uptimeSeconds / 60) - (60 * Math.floor(Uptime.uptimeSeconds / 60 / 60)))} minute(s) and ${Math.floor(Uptime.uptimeSeconds - (60 * Math.floor(Uptime.uptimeSeconds / 60)))} second(s)`
+        return `The bot has been up for ${Math.floor(Uptime.uptimeSeconds / 60 / 60 / 24)} day(s), ${Math.floor(Uptime.uptimeSeconds / 60 / 60) - (24 * Math.floor(Uptime.uptimeSeconds / 60 / 60 / 24))} hour(s), ${Math.floor((Uptime.uptimeSeconds / 60) - (60 * Math.floor(Uptime.uptimeSeconds / 60 / 60)))} minute(s) and ${Math.floor(Uptime.uptimeSeconds - (60 * Math.floor(Uptime.uptimeSeconds / 60)))} second(s)`
     }
 }
