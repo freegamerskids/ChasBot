5ee6e6d68b98cf2d6a3e4dc88d505306be227406
freegamerskids <freegamerskids@gmail.com>
2023-08-10 08:28:56 +0200

    fixed /fun sillycat

--- src/cmds/fun/[silly].ts
+++ src/cmds/fun/[silly].ts
@@ -2,44 +2,41 @@ import { ChasBot } from "../../typings/ChasBot";
 import { CommandInteraction, CommandInteractionOptionResolver, EmbedBuilder, ColorResolvable } from "discord.js";
 import { request } from "undici";
 
+let useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 RuxitSynthetic/1.0 v18911347667 t7338451470782258495 athfa3c3975 altpub cvcv=2 smf=0'
+
 export default {
     name:'silly',
     description:'so silly',
     options:[],
-    async run(i: CommandInteraction, options: CommandInteractionOptionResolver, c: ChasBot) {
+    async run(i: CommandInteraction, options: CommandInteractionOptionResolver) {
         const commands = require('./sillyConfig.json')
         const commandName = options.getSubcommand()
         const cmd = commands[commandName.toLowerCase()]
 
         const multi = cmd['multireddits'][Math.floor(Math.random() * cmd['multireddits'].length)]
-        let subreddit:string
+        let subreddit
         {
-            let req = await request('https://api.reddit.com/api/multi'+multi)
+            let req = await request(`https://api.reddit.com/api/multi${multi}?raw_json=1`,{headers:{'User-Agent':useragent}})
             let json = await req.body.json()
             const subreddits: {name:string}[] = json.data.subreddits
             subreddit = 'r/' + subreddits[Math.floor(Math.random() * subreddits.length)].name
         }
-        
-        let post:{author:string,title:string,media:string,link:string,embed_color:string}
-        {
-            let req = await request(`https://api.reddit.com/${subreddit}/random`, {maxRedirections:1})
-            let json = await req.body.json()
-            const api_post = json[0].data.children[0].data
 
-            post = {
-                author: api_post.author,
-                title: api_post.title,
-                media: api_post.media ? api_post.media.reddit_video.fallback_url : api_post.url,
-                link: 'https://reddit.com'+api_post.permalink,
-                embed_color: api_post.link_flair_background_color
-            }
-        }
+        let req = await request(`https://api.reddit.com/${subreddit}/random?raw_json=1`, {maxRedirections:1,headers:{'User-Agent':useragent}})
+        let json = await req.body.json()
+        const post = json[0].data.children[0].data
+
+        const embed_col = post.link_flair_background_color
+            ? post.link_flair_background_color
+            : (post.author_flair_text_color
+                ? post.author_flair_text_color
+                : 'Random')
 
         const embed = new EmbedBuilder()
         .setTitle(post.title)
-        .setAuthor({name:`By ${post.author} on ${subreddit}`,url:post.link})
-        .setImage(post.media)
-        .setColor(post.embed_color as ColorResolvable)
+        .setAuthor({name:`By ${post.author} on ${subreddit}`,url:'https://reddit.com'+post.permalink})
+        .setImage(post.is_video ? post.media.reddit_video.fallback_url : post.url.replace('gifv','gif'))
+        .setColor(embed_col == 'dark' ? 'DarkButNotBlack' : embed_col as ColorResolvable)
 
         return await i.reply({ embeds:[embed] })
     }
